cb0ed3ce7567c442b59c98d5d4be8a61595550f5
zhzenghui <zhzenghui@gmail.com>
2015-09-25 13:08:08 +0800

    截图

--- TestPro/ViewController.m
+++ TestPro/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
@@ -14,14 +15,51 @@
 
 @implementation ViewController
 
+#pragma mark - view cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self still];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - video
+
+- (void)saveImage:(UIImage *)image {
+    
+    
+    
+}
+
+- (void)still {
+    
+    NSString *movpath =[[NSBundle mainBundle] pathForResource:@"1938" ofType:@"mp4"];
+    NSURL *url = [NSURL fileURLWithPath:movpath];
+    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:url options:nil];
+    
+    AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+    
+    NSError *error;
+    CMTime actualTime;
+    
+    
+    CMTime currentTime = CMTimeMakeWithSeconds(1, 600);
+
+    CGImageRef currentImage = [imageGenerator copyCGImageAtTime:currentTime
+                                                     actualTime:&actualTime
+                                                          error:&error];
+    
+    if (!error) {
+        UIImage *image = [[UIImage alloc] initWithCGImage:currentImage];
+        
+        self.view.backgroundColor = [UIColor colorWithPatternImage:image];
+    }
 }
 
 @end
